6c779938dfc3fea303f191c67d20c61920c08d4a
pasuta <pastalexs@gmail.com>
2018-10-15 09:22:56 +0700

    add test class

---
+++ app/src/main/java/com/test/testgame/model/event/TestClass.java
@@ -0,0 +1,17 @@
+package com.test.testgame.model.event;
+
+public class TestClass {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public TestClass() {
+    }
+}
